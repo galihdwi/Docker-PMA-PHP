78d067ba4cdbbb3e885573103fc7e25f26d924c1
Galih Dwi <me.oceaner@gmail.com>
2021-01-24 09:18:14 +0800

    initialized

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM php:7.4-cli
+RUN apt-get update && apt-get install -y mysql-client nano
+RUN docker-php-ext-install mysqli pdo pdo_mysql
+RUN docker-php-ext-enable mysqli
+COPY . /usr/src/myapp
+WORKDIR /usr/src/myapp
+CMD [ "php", "./index.php" ]
---
+++ php/index.php
@@ -0,0 +1,10 @@
+<?php
+
+$db_host        = '172.20.0.2';
+$db_user        = 'root';
+$db_pass        = 'mypassword';
+$db_database    = 'dump'; 
+$db_port        = '3306';
+
+echo "hello";
+$link = mysqli_connect($db_host,$db_user,$db_pass,$db_database,$db_port);
